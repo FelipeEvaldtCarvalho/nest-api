8fc3a8b85a41bad63a5004e14423e278b9caa363
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-06-03 23:49:45 -0300

    refactor: reorganizar imports no módulo de clientes

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
-
+import { Customer } from './entities/customer.entity';
+import { User } from 'src/users/entities/user.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Customer, User])],
   controllers: [CustomersController],
   providers: [CustomersService],
 })
